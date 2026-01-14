c3cbbd6d433999a3de55ef374d128304c2431e74
kowhlwa <hyunssl101@gmail.com>
2026-01-13 23:16:24 -0500

    finished 09_sumall

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+        return "ERROR";
+    }
+    let dir = (a < b) ? 1 : -1;
+    let sum = b;
+    for (let i = a; i != b; i += dir) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
