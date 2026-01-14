919ad4162ad3c924cb06cbda4c2d3f525bbddebc
kowhlwa <hyunssl101@gmail.com>
2026-01-13 23:29:17 -0500

    Finished 11_tempconversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  return Math.round((5/9) * (fahr - 32) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  return Math.round((cels * (9/5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
