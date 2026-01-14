85dbfa76150598dd2126596dfa65c563a8424276
kowhlwa <hyunssl101@gmail.com>
2026-01-13 23:01:07 -0500

    finished 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
 const removeFromArray = function() {
+    let arr = [];
+    for (const arg of arguments) {
+        if (Array.isArray(arg)) {
+            arr = arg;
+        } else {
+            let removeIndex = arr.lastIndexOf(arg);
+            while (removeIndex != -1) {
+                arr.splice(removeIndex, 1);
+                removeIndex = arr.lastIndexOf(arg);
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
