02b9a1b677b0b65bd4b1a2a6e031dd771166deec
kowhlwa <hyunssl101@gmail.com>
2026-01-20 18:48:29 -0500

    finished fibonacci 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,27 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num == 0) {
+        return 0;
+    } else if (num < 0) { 
+        return "OOPS"
+    }
+    let left = 0;
+    let right = 1;
+    let temp;
+    for (let i = 1; i < num; i++) {
+        temp = right;
+        right = right + left;
+        left = temp;
+    }
+    return right;
 };
+
+/**
+ * 
+ * fibonacci(1) --> 1
+ * fibonacci(2) --> 1
+ * fibonacci(3) --> 2
+ * ...
+ */
 
 // Do not edit below this line
 module.exports = fibonacci;
