80f779fb917b9c54e64c0112001b729c8863df21
kowhlwa <hyunssl101@gmail.com>
2026-01-20 22:06:23 -0500

    finished all foundational javascript exercises!

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        let oldestAge = (oldest.yearOfDeath) ? oldest.yearOfDeath - oldest.yearOfBirth : 2026 - oldest.yearOfBirth;
+        let personAge = (person.yearOfDeath) ? person.yearOfDeath - person.yearOfBirth : 2026 - person.yearOfBirth;
+        return (oldestAge > personAge) ? oldest : person;
+    });
 };
 
 // Do not edit below this line
